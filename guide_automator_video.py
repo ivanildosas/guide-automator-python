4037b59e2cc5730a48b3f6e2c73143511c7ae8c3
Guilherme <guidovalle1608@gmail.com>
2018-11-14 19:59:27 -0300

    Adiciona função speak no módulo de vídeos

--- guide_automator_video.py
+++ guide_automator_video.py
@@ -1,10 +1,18 @@
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
-from guide_automator_function import *
+import pyttsx
+
+
 # Move mouse to element
 def clickOnElement(x, y):
     mouse = PyMouse()
     mouse.move(x, y)
     mouse.click(x, y, 1)
 
+# Speak some text
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    
 
